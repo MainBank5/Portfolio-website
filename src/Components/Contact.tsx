3f29637311e023d36c862018e44815a7505df763
Eliud Karuga <eliudkaruga97@gmail.com>
2024-01-24 15:46:40 +0300

    updated text area

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -15,7 +15,7 @@ const Contact = () => {
         <form method="POST" className="flex flex-col w-3/5 h-1/2" action="https://getform.io/f/24139a13-3c0b-448f-86ea-6e60b5be9775" >
           <input name="name" className=" text-black shadow-cyan-500 shadow-sm p-3 rounded-md mt-4" type="text" required placeholder="First Name.." /> <br />
           <input name="email" className=" text-black shadow-cyan-500 shadow-sm  rounded-md  p-3 mt-4 " type="email" required placeholder="email address.." />  <br />
-          <textarea name="message" className=" shadow-sm shadow-cyan-400 p-3 mt-4 w-full h-[150px] rounded-md resize-none" placeholder="Message.."  ></textarea><br />
+          <textarea name="message" className="text-black shadow-sm shadow-cyan-400 p-3 mt-4 w-full h-[150px] rounded-md resize-none" placeholder="Message.."  ></textarea><br />
           <button className="bg-blue-500 mt-5 text-center text-2xl font-bold rounded-lg">Submit</button>
         </form>
       </div>
